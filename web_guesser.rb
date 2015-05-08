7b2da15e12f9c5eac70b7d7818df1344b863e689
Erik Butcher <with_a_k@mac.com>
2015-05-08 15:01:03 -0600

    I5: guess limiter enabled

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,7 +2,39 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 var = rand(101)
+message = "Give it a try."
+counter = 0
 
 get '/' do
-  "The secret number is: #{var}"
+  if params['guess'].to_i > var + 5
+    message = "It's much too high."
+    color = 'red'
+    counter += 1
+  elsif params['guess'].to_i < var - 5
+    message = "It's much too low."
+    color = 'red'
+    counter += 1
+  elsif params['guess'].to_i > var
+    message = "It's too high."
+    color = 'salmon'
+    counter += 1
+  elsif params['guess'].to_i < var
+    message = "It's too low."
+    color = 'salmon'
+    counter += 1
+  elsif params['guess'].to_i == var
+    message = "The secret number is #{var}. It's been changed."
+    color = 'green'
+    var = rand(101)
+    counter = 0
+  end
+  if counter > 4
+    message = "You're out of tries. The number has changed."
+    color = "rgb(150,150,255)"
+    var = rand(101)
+    counter = 0
+  end
+  erb :index, :locals => {:number => var,
+                          :message => message,
+                          :color => color}
 end
